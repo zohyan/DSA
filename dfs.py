30cd9d1202f69b6bc7fda18bfce84278398c7871
RAHARIJAONA Zo Lalaina Yannick <rzolalainayannick@gmail.com>
2021-06-21 22:10:49 -0400

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -8,7 +8,11 @@ class Node:
         return self
 
     def depthFirstSearch(self, array):
-        # Write your code here.
+        # Input : Node
+        # Output : array is the result
+        # Add the current_node to the "array" and call 
+        # depthFirstSearch for every child of this current_node.
+        
         array.append(self.name)
         for child in self.children:
             child.depthFirstSearch(array)
